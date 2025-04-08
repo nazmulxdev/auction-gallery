2d3c440874987ed8574ceb070b672c7befe155ab
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-04-08 23:03:03 +0600

    added all assets and initiaized the project

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import Swal from "sweetalert2";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+
+// custom sweet alert
+// Swal.fire({
+//   position: "top-end",
+//   icon: "success",
+//   title: "Your work has been saved",
+//   showConfirmButton: false,
+//   timer: 1500
+// });
+
+// heart icon
+{
+  /* <FaHeart className="text-red-500"/> */
+}
+
+function App() {
+  return (
+    <>
+      <h1 className="sora text-2xl cursor-pointer">nazmul</h1>
+      <button className="poppins btn btn-primary">sell naw</button>
+    </>
+  );
+}
+
+export default App;
